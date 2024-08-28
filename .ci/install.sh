06502e0ef02175af8e6712affb544a1b6ce56c61
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2024-08-29 01:24:53 +0200

    Improved run

--- .ci/install.sh
+++ .ci/install.sh
@@ -32,7 +32,7 @@ python3 -m pip install coverage
 python3 -m pip install defusedxml
 python3 -m pip install olefile
 python3 -m pip install -U pytest
-python3 -m pip install -U pytest-cov
+python3 -m pip install -U pytest-xdist
 python3 -m pip install -U pytest-timeout
 python3 -m pip install pyroma
 
